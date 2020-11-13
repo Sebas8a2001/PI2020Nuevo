70ad7b9ab2aac58ba3ffc2d8ed4eade633a5f44a
Kevin Cubillos <kevinlexr@gmail.com>
2020-11-12 19:10:01 -0500

    Diseño perfil

--- app/src/main/java/com/example/pi2020/MainActivity.java
+++ app/src/main/java/com/example/pi2020/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Login.class);
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);
             }
         });
